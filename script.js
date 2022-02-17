138fe1caefd27215187a74b78c42c3f6b9a730e8
Cihsan <mutamuty@gmail.com>
2022-02-17 22:55:50 +0600

    final

--- script.js
+++ script.js
@@ -1,80 +1,66 @@
-
-
-/* function parseDotValue(id) {
-    let idname = document.getElementById(id).parseInt().value
+function parseDotValue(idv) {
+    let idnamev = document.getElementById(idv)
+    let idnameConvert = parseInt(idnamev.value)
+    return idnameConvert
 }
+function parseDotInner(idi) {
+    let idname = document.getElementById(idi)
+    let idnameCon = parseInt(idname.innerText)
+    return idnameCon
+}
+
 document.getElementById('calculate').addEventListener('click', function () {
-    //all input
-    let income = document.getElementById('income');
-    let incomeConvert = parseInt(income.value);
-    //parseAndValue('income')
-    let food = document.getElementById('food');
-    let foodConvert = parseInt(food.value);
-    //parseAndValue('food')
-    let rent = document.getElementById('rent');
-    let rentConvert = parseInt(rent.value);
-    //parseAndValue('rent')
-    let etc = document.getElementById('etc');
-    let etcConvert = parseInt(etc.value);
-    //parseAndValue('etc')
-    let expense = foodConvert + rentConvert + etcConvert;
+    let getIncome = parseDotValue('income');
+    let getFood = parseDotValue('food');
+    let getRent = parseDotValue('rent');
+    let getEtc = parseDotValue('etc');
+    let expenseTotal = getFood + getRent + getEtc;
+    let showTotal = parseDotInner('totalExpense');
+    let getBalance = parseDotInner('balance');
+    let balanceTotal = getIncome - expenseTotal;
 
-
-    //let expense = (parseAndValue('food')) + (parseAndValue('rent')) + (parseAndValue('etc'))
-    //update here
-    let totalExpense = document.getElementById('totalExpense');
-    let totalExpenseConvert = parseInt(totalExpense.innerText);
-
-
+    let totalExpense = document.getElementById('totalExpense')
+    totalExpense.innerText = expenseTotal;
     let balance = document.getElementById('balance')
-    let balanceConvert = parseInt(balance.innerText);
-    let expenseAfter = incomeConvert - expense;
+    balance.innerText = balanceTotal;
 
-    // if (incomeConvert > 0 && foodConvert > 0 && rentConvert > 0 && etc > 0) {
-    if (0 < incomeConvert && 0 < foodConvert && 0 < rentConvert && 0 < etc) {
-        document.getElementById('message').style.display = 'block'
+    //Validation Done For Negetive value 
+    if (getIncome > 0 && getFood > 0 && getRent > 0 && getEtc > 0) {
+        document.getElementById('warnningOne').style.display = 'none'
     }
     else {
-        document.getElementById('message').style.display = 'none'
+        document.getElementById('warnningOne').style.display = 'block'
     }
-
-    if (expense < expenseAfter) {//&& expenseAfter > expense
-        totalExpense.innerText = expense
-        balance.innerText = expenseAfter
-        document.getElementById('messageForMoreExpense').style.display = 'none'
+    //Validation Done For Expense more than Income
+    if (expenseTotal < balanceTotal) {
+        document.getElementById('warnningTwo').style.display = 'none';
     }
     else {
-        document.getElementById('messageForMoreExpense').style.display = 'block'
+        document.getElementById('warnningTwo').style.display = 'block';
     }
-
 
 })
 
 document.getElementById('save').addEventListener('click', function () {
-    let income = document.getElementById('income')
-    let incomeConvert = parseInt(income.value)
-    let savePercent = document.getElementById('savePercent')
-    let savePercentConvert = parseInt(savePercent.value)
-    let getPercent = (incomeConvert * savePercentConvert) / 100
+    let getIncome = parseDotValue('income');
+    let getPercent = parseDotValue('savePercent');
+    let getPercentTotal = (getIncome * getPercent) / 100;
     //saving amount
-    let savingAmount = document.getElementById('savingAmount')
-    let savingAmountConvert = parseInt(savingAmount.innerText)
-    //savingAmount.innerText = getPercent
-    console.log(savePercentConvert)
-    let remainBalance = document.getElementById('remainBalance')
-    let remainBalanceConvert = parseInt(remainBalance.innerText)
-    let balance = document.getElementById('balance')
-    let balanceConvert = parseInt(balance.innerText);
-    let remain = balanceConvert - getPercent
-    if (getPercent < balanceConvert) {
-        savingAmount.innerText = getPercent
-        remainBalance.innerText = remain;
-        document.getElementById('message2').style.display = 'none'
+    let showSaveAmount = parseDotInner('savingAmount');
+    console.log(getPercentTotal);
+    let showRemainBalance = parseDotInner('remainBalance');
+    let getBalance = parseDotInner('balance');
+    let remainTotal = getBalance - getPercentTotal;
+
+    if (getPercentTotal < getBalance) {
+        let saveAmountshow = document.getElementById('savingAmount')
+        saveAmountshow.innerText = getPercentTotal
+        let remainBalanceShow = document.getElementById('remainBalance')
+        remainBalanceShow.innerText = remainTotal
+        document.getElementById('warnningThree').style.display = 'none'
     }
     else {
-        document.getElementById('message2').style.display = 'block'
+        document.getElementById('warnningThree').style.display = 'block'
     }
-    //remainBalance.innerText = remain;
-})
- */
 
+})
